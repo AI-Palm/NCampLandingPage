f6cc0fa5c50b6e88f1c167acdbd6b6424ef82eec
tony <tranquockhanhdhsp99@gmail.com>
2024-05-03 14:53:45 +0700

    Header banner done

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import Header from "@/components/Header/page";
+import Footer from "@/components/Footer/page";
 
 export default function Home() {
   return (
     <div>
-      NCAMP
-      <Button>Click me</Button>
+      <Header />
+      <Footer />
 
     </div>
   );
