aaba57bbe69de4576c0df09c28a4782f68161767
tony <tranquockhanhdhsp99@gmail.com>
2024-05-07 17:32:12 +0700

    motion for section 2 5 12

--- components/Section12/Section12.jsx
+++ components/Section12/Section12.jsx
@@ -1,17 +1,64 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useRef } from "react";
 import Image from 'next/image'
 import AtmCard from "@/public/images/atm-card.png"
 import ArrowIcon from "@/public/icons/arrow-icon.png"
+import { useInView } from "react-intersection-observer";
 
-const Section12 = () => {
+import { motion, useScroll, useAnimation } from "framer-motion";
+
+import useInViewAnimation from "@/components/useInViewAnimation";
+
+const Section12 = () => {   
+    const { ref: h1Ref, controls: h1Controls } = useInViewAnimation();
+    const { ref: pRef, controls: pControls } = useInViewAnimation();
+    const { ref: h1blRef, controls: h1blControls } = useInViewAnimation();
+    const { ref: descRef, controls: descControls } = useInViewAnimation();
+
+
+      
+    
+
   return (
     <>
-        <div className="bg-orange-400 h-full flex justify-center  " >
+        <div className="bg-orange-400 h-full flex justify-center  "  >
             <div className='flex flex-col  justify-center items-center my-10'>
-                <h1 className='text-white text-4xl mb-8 font-extrabold'>신청방법</h1>
-                <p className='text-white font-extrabold mb-8'><span className='text-orange-200 font-extrabold'>국민내일배움카드 소지자는</span> 교육비 전액 무료 지원<br />
-                    <span className='text-orange-200 font-extrabold'>매월</span> 지원훈련 장려금도 지급</p>
-                <div className='flex  flex-wrap md:flex-row justify-center w-auto m-4 '>
+                <motion.h1 
+                    ref={h1Ref}
+                    initial={{ opacity: 0, x: 0 }}
+                    animate={h1Controls}
+                    transition={{
+                      type: "spring",
+                      stiffness: 360,
+                      damping: 100,
+                    }}
+                    className='text-white text-4xl mb-20 font-extrabold translate-x-1/2 '
+                    >
+                    신청방법
+                </motion.h1>
+                <motion.p 
+                    ref={pRef}
+                    initial={{ opacity: 0, x: 0 }}
+                    animate={pControls}
+                    transition={{
+                      type: "spring",
+                      stiffness: 360,
+                      damping: 100,
+                    }}
+                    className='text-white font-extrabold mb-8'>
+                    <span className='text-orange-200 font-extrabold'>국민내일배움카드 소지자는</span> 교육비 전액 무료 지원<br />
+                    <span className='text-orange-200 font-extrabold'>매월</span> 지원훈련 장려금도 지급</motion.p>
+                <motion.div 
+                initial={{ opacity: 0, y: "180px" }}
+                whileInView={{ opacity: 1, y: 0}}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 80,
+                    duration:3
+                   
+                  }}
+                className='flex  flex-wrap md:flex-row justify-center w-auto m-4 '>
                     <div className='flex flex-col justify items-center'>
                         <div className='flex flex-row items-center'>
                             <div className="rounded-full bg-white px-6 py-8  my-4 ml-4">
@@ -51,17 +98,36 @@ const Section12 = () => {
                         </div>
                         <span className='text-white mb-6'>최종합격<br/>      </span>
                     </div>
-                </div>
+                </motion.div>
 
-                <h1 className='text-white text-4xl mt-20 mb-8 font-extrabold'>혜택 및 지원자격</h1>
-                <div className='flex flex-col justify-around '>
-                    <div className="flex flex-col items-center lg:flex-row">
+                <motion.h1
+                    ref={h1blRef}
+                    initial={{ opacity: 0, x: 0 }}
+                    animate={h1blControls}
+                    transition={{
+                      type: "spring",
+                      stiffness: 360,
+                      damping: 100,
+                    }}
+                className='text-white text-4xl mt-20 mb-8 font-extrabold'>혜택 및 지원자격</motion.h1>
+                <motion.div 
+                    ref={descRef}
+                    initial={{ opacity: 0, x: 0 }}
+                    animate={descControls}
+                    transition={{
+                      type: "spring",
+                      stiffness: 360,
+                      damping: 100,
+                    }}
+
+                    className='flex flex-col justify-around '>
+                    <div className="flex flex-col lg:items-center items-start lg:flex-row">
                         <ul className="text-sm lg:text-xl md:text-xl text-white">
                             <li>1. 내일배움카드 발급을 하셔야 100% 지원</li>
                             <li>2. 오전9시 ~ 오후 6시까지 수업 참여가 가능</li>
                             <li>3. 취업을 목표로 하는 미취업자, 대학 재 · 휴학 및 졸업 예정자</li>
                         </ul>
-                        <div className="hidden lg:block max-w-xs ml-8">
+                        <div className="block max-w-xs my-4 lg:my-0 lg:ml-8">
                             <Image src={AtmCard} alt="ATM Card Image" width={200} height={200} />
                         </div>
                     </div>
@@ -71,7 +137,7 @@ const Section12 = () => {
                         <li>3년제 : 1학년 2학기 종료시점부터 지원 가능</li>
                         <li>2년제 : 입학 시점부터 지원 가능</li>
                     </ul>
-                </div>
+                </motion.div>
 
                 
                
--- components/Section2/Section2.jsx
+++ components/Section2/Section2.jsx
@@ -8,23 +8,19 @@ import { useRef } from "react";
 const Section2 = () => {
     const containerRef = useRef();
 
-    const { scrollYProgress } = useScroll({ container: containerRef });
 
     const skillRef = useRef();
-    // const isSkillRefInView = useInView(skillRef, {once:true});
     const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
 
-    const experienceRef = useRef();
-    const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" })
     return (
         <>
-        <div className="bg-black h-screen flex justify-center items-center " ref={containerRef}>
+        <div className="bg-black h-full flex justify-center items-center " ref={containerRef}>
             <motion.div 
                 initial={{opacity: 0, y: "200px" }}
                 animate={{opacity: isSkillRefInView ? 1 : 0, y: isSkillRefInView ? 0 : 20   }}
                 transition={{ delay: 0 }}
-                className="flex flex-row  justify-center items-center">
-                <div className=" w-auto h-80 bg-black border-2 border-white rounded-3xl p-4 m-4 text-white flex flex-col sm:w-auto md:w-auto lg:w-80 lg:h-80 md:h-80 sm:h-80"  ref={skillRef}>
+                className="flex  flex-col md:flex-row  justify-center items-center">
+                <div className=" w-full h-80 bg-black border-2 border-white rounded-3xl p-4 m-4 text-white flex flex-col sm:w-full md:w-full lg:w-80 lg:h-80 md:h-80 sm:h-80"  ref={skillRef}>
                     <h1 className="text-xl lg:text-4xl md:text-4xl text-orange-500 mt-10 mb-4 text-left">
                         교육일정
                     </h1>
@@ -38,7 +34,7 @@ const Section2 = () => {
                         <li>수강료 :<span className='line-through decoration-orange-500'>6,473,520원</span>  → 0원</li>
                     </ul>
                 </div>
-                <div className="w-auto h-80 bg-black border-2 border-white rounded-3xl p-4 m-4 text-white flex flex-col sm:w-auto md:w-auto xl:w-80 lg:h-80 md:h-80 sm:h-80 xs:h-48 "  ref={skillRef}>
+                <div className="w-full h-80 bg-black border-2 border-white rounded-3xl p-4 m-4 text-white flex flex-col sm:w-full md:w-full xl:w-80 lg:h-80 md:h-80 sm:h-80 xs:h-48 "  ref={skillRef}>
                     <h1 className="text-xl lg:text-4xl md:text-4xl text-orange-500 mt-10 mb-4 text-left">
                         교육지원
                     </h1>
