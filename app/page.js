0f08f16d5f04e842a028056c92123bab10e1bf7c
kenichi <baold201@gmail.com>
2024-05-21 16:42:31 +0700

    structure folder

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
-import Section3 from "@/components/ui/Section3";
-import Slider from "@/components/ui/Slider";
+import Section3 from "@/components/Section3/Section3";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Section4 from "@/components/ui/Section4";
+import Section4 from "@/components/Section4/Section4";
 import Section2 from "@/components/Section2/Section2";
 import Section5 from "@/components/Section5/Section5";
 import Section12 from "@/components/Section12/Section12";
 import Section8 from "@/components/Section8/Section8";
 import Section9 from "@/components/Section9/Section9";
 import Section10 from "@/components/Section10/Section10";
-import Section6 from "@/components/ui/Section6";
-import Section7 from "@/components/ui/Section7";
+import Section6 from "@/components/Section6/Section6";
+import Section7 from "@/components/Section7/Section7";
 import Section11 from "@/components/Section11/Section11";
 
 export default function Home() {
